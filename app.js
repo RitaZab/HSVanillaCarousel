e8e02f1ba10e0a444a28db76609f8c4661d3cd06
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-11-19 18:05:18 +0100

    Adding continents names

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 //Creating global variables
 let arrowleft = document.querySelector("#arrow-left");
 let arrowright = document.querySelector("#arrow-right");
-let continentname = document.querySelector(".continentname");
+let continentname = document.querySelector(".continentname").textContent;
 let activePhotoNr = 1
+
+let namingContinent = () => {
+    switch (activePhotoNr) {
+        case 1:
+            document.querySelector(".continentname").innerText= "Azji"
+            break
+        case 2:
+            document.querySelector(".continentname").innerText= "Afryki"
+            break
+        case 3:
+            document.querySelector(".continentname").innerText= "Ameryki Północnej"
+            break
+        case 4:
+            document.querySelector(".continentname").innerText= "Ameryki Południowej"
+            break
+        case 5:
+            document.querySelector(".continentname").innerText= "ziem Antarktydy"
+            break
+        case 6:
+            document.querySelector(".continentname").innerText= "Europy"
+            break
+        case 7:
+            document.querySelector(".continentname").innerText= "Australii"
+            break
+    }
+    
+}
 //We need to remove class active from actual one to make it possible to other image to get it solely
 let removeActive = () => {
     let activephoto = document.querySelector(".active");
@@ -11,7 +38,9 @@ let removeActive = () => {
 //Adding class active to actually show photo on the page
 let showphoto = (photoNumber) => {
     removeActive();
-    document.querySelector("#photo"+photoNumber).classList.add("active");
+    document.querySelector("#photo" + photoNumber).classList.add("active")
+    namingContinent()
+        
 }
 //Next to function are for arrow, they work by changing actual photo nr
 let nextSlide = () => {
@@ -22,6 +51,7 @@ let nextSlide = () => {
         activePhotoNr += 1;
     }
     showphoto(activePhotoNr);  
+    
 }
 
 let prevoiusSlide = () => {
@@ -39,9 +69,13 @@ for (let i = 1; i <= 7; i++){//This function is for using dots to give actual ph
     let showPhotoNr = () => {
         activePhotoNr = i
         showphoto(i)
+        
     }
-    document.querySelector("#dot"+i).addEventListener("click",showPhotoNr)
+    document.querySelector("#dot" + i).addEventListener("click", showPhotoNr)
+   
 }
 
 arrowleft.addEventListener("click",prevoiusSlide);
-arrowright.addEventListener("click",nextSlide)
+arrowright.addEventListener("click", nextSlide);
+
+
